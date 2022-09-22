b27d800e6d0e31dddb15d21ac7d021137ce42e83
xumaxa <56018430+xumaxa@users.noreply.github.com>
2022-09-22 10:54:56 +0200

    Create ejercicio2.php

---
+++ ejerciciosPrincipiantes/ejercicio2.php
@@ -0,0 +1,23 @@
+<?php 
+    //entero
+    $n= 10;
+    echo $n. "<p>\n</p>";
+    //octal
+    $n = 010;
+    echo $n. "<p>\n</p>";
+    //hexadecimal
+    $n= 0x10;
+    echo $n. "<p>\n</p>";
+    //float
+    $n=20.4;
+    echo $n. "<p>\n</p>";
+    //float con notacion flotante
+    $n =20.4E1;
+    echo $n. "<p>\n</p>";
+    //cadena
+    $n = "hola";
+    echo $n. "<p>\n</p>";
+    //booleano
+    $n = true;
+    echo $n. "<p>\n</p>";
+?>
